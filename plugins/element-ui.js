9d8978efbea48a40014c08115c2acd461a082455
talog <tee.talog@gmail.com>
2018-02-14 17:40:22 +0900

    element config

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 
+import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/display.css'
 
-const locale = require('element-ui/lib/locale/lang/ja')
-require('element-ui/lib/theme-chalk/index.css')
+import locale from 'element-ui/lib/locale/lang/ja'
 
 Vue.use(ElementUI, { locale })
 
