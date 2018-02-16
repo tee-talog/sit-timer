3a8f52291303f47b08b98a5129bcba23a54c37dc
talog <tee.talog@gmail.com>
2018-02-16 23:17:10 +0900

    add header

---
+++ components/SitHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="header-section">
+    <h1 class="header-title">
+      <nuxt-link to="/">SIT-timer</nuxt-link>
+    </h1>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.header-section {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+}
+</style>
+
