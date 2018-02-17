60ee3fba12064dfcd4d74ba0429505c2dfbb4153
talog <tee.talog@gmail.com>
2018-02-18 01:46:18 +0900

    create error page

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="error-section">
+    <h1 v-if="error.statusCode === 404">ページが見つかりません</h1>
+    <h1 class="error-content" v-else>エラーが発生しました</h1>
+    <el-button class="error-content" type="text" @click="handleClickHomeButton">トップ</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  methods: {
+    handleClickHomeButton () {
+      this.$router.push({ path: '/' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.error-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+}
+.error-content {
+  margin: 20px;
+}
+</style>
