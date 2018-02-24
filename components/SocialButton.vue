7a5a493eaef5b825d82ee4888d2089a160c668d0
talog <tee.talog@gmail.com>
2018-02-24 19:13:23 +0900

    specify url

--- components/SocialButton.vue
+++ components/SocialButton.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-      class="twitter-share-button"
-      data-text="トレーニング中！"
-      data-lang="ja"
-      data-show-count="false"
-    >Tweet</a>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <div class="twitter">
+      <a
+        href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+        class="twitter-share-button"
+        data-text="トレーニング中！"
+        data-url="https://tee-talog.github.io/sit-timer/"
+        data-lang="ja"
+        data-show-count="false"
+      >Tweet</a>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
   </div>
 </template>
 
