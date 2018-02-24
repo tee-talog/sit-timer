e2b2b7fb5d4a1327f20769dfdfff4711fff785ae
talog <tee.talog@gmail.com>
2018-02-24 19:10:13 +0900

    create social button component

---
+++ components/SocialButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+      class="twitter-share-button"
+      data-text="トレーニング中！"
+      data-lang="ja"
+      data-show-count="false"
+    >Tweet</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
+
