eac07b4d54cddc1b91796a96cba6b313d1a0d2c6
talog <tee.talog@gmail.com>
2018-02-27 23:16:06 +0900

    button styling

--- components/SocialButton.vue
+++ components/SocialButton.vue
@@ -7,15 +7,21 @@
       inline-template
     >
       <div class="g_social-sharing-buttons">
-        <div class="g_social-sharing-buttons-button g_social-sharing-buttons-button-twitter">
-          <network network="twitter">Twitter</network>
-        </div>
-        <div class="g_social-sharing-buttons-button g_social-sharing-buttons-button-facebook">
-          <network network="facebook">Facebook</network>
-        </div>
-        <div class="g_social-sharing-buttons-button g_social-sharing-buttons-button-line">
-          <network network="line">LINE</network>
-        </div>
+        <network network="twitter" class="g_social-sharing-buttons-button-link">
+          <span class="g_social-sharing-buttons-button g_social-sharing-buttons-button-twitter">
+            Twitter
+          </span>
+        </network>
+        <network network="facebook" class="g_social-sharing-buttons-button-link">
+          <span class="g_social-sharing-buttons-button g_social-sharing-buttons-button-facebook">
+            Facebook
+          </span>
+        </network>
+        <network network="line" class="g_social-sharing-buttons-button-link">
+          <span class="g_social-sharing-buttons-button g_social-sharing-buttons-button-line">
+            LINE
+          </span>
+        </network>
       </div>
     </social-sharing>
   </div>
@@ -45,11 +51,36 @@ export default {
 }
 .g_social-sharing-buttons-button {
   margin: 0 5px;
+  border-radius: 5px;
+  height: 2em;
+  width: 5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .g_social-sharing-buttons-button-twitter {
+  background-color: #1da1f2;
+  color: #FFF;
 }
 .g_social-sharing-buttons-button-facebook {
+  background-color: #3b5998;
+  color: #FFF;
 }
 .g_social-sharing-buttons-button-line {
+  background-color: #00b900;
+  color: #FFF;
+}
+.g_social-sharing-buttons-button-link {
+  vertival-align: middle;
+  display: flex;
+  align-items: center;
+  flex-basis: 5em;
+  /*
+  position: relative;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  */
 }
 </style>
