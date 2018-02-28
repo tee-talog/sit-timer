ace18cb248b14f4ca1ce32c65583e49f1ed3ec5b
talog <tee.talog@gmail.com>
2018-02-28 22:44:52 +0900

    add subtitle

--- pages/about.vue
+++ pages/about.vue
@@ -3,6 +3,7 @@
     <h1 class="page-title">当サイトについて</h1>
     <h2 class="page-subtitle">免責事項</h2>
     <p class="paragraph disclaimer">{{ disclaimer }}</p>
+    <h2 class="page-subtitle">連絡先</h2>
     <address class="address">
       <div>Twitter:<a href="https://twitter.com/tee_talog">@tee-talog</a></div>
     </address>
