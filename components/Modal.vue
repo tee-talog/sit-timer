3b10bd9d284272f7e530cbbef3a1fd2935885414
talog <tee.talog@gmail.com>
2018-05-03 14:30:27 +0900

    create Modal component

---
+++ components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="modal-section">
+    <SocialButton/>
+  </div>
+</template>
+
+<script>
+import SocialButton from '~/components/SocialButton.vue'
+
+export default {
+  components: {
+    SocialButton
+  }
+}
+</script>
+
+<style scoped>
+.modal-section {
+  position: fixed;
+}
+</style>
+
